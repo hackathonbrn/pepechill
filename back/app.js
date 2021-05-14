1bd07cdd8a32e155a9c2ebb3d318db3a4852873d
Daniil Antsupov <antsupov.official@gmail.com>
2021-05-14 23:39:44 +0700

    Add mongodb connection to project

--- back/app.js
+++ back/app.js
@@ -1,13 +1,30 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const MongoClient = require('mongodb').MongoClient;
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+require('dotenv').config();
 
-var app = express();
+const mongoClient = new MongoClient(process.env.MONGO_URL, { useUnifiedTopology: true });
+
+console.log('connection');
+
+mongoClient.connect(async (err, client) => {
+  const db = client.db('pepechill');
+
+  const collection = db.collection('users');
+  let data = await collection.find().toArray();
+
+  console.log(data);
+  client.close();
+});
+
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -23,12 +40,12 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -38,4 +55,4 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-module.exports = app;
+app.listen(3022);
