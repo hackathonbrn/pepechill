3c6d4ed949379474e335e3ee64363bfc6af36cb3
Andrey <andrey.grasev@gmail.com>
2021-05-15 18:32:29 +0700

    dev1 Add style activities-item

--- front/src/components/activities-item/activities-item.js
+++ front/src/components/activities-item/activities-item.js
@@ -2,12 +2,14 @@ import React from 'react';
 
 import { Panel, Progress, Icon, FlexboxGrid } from 'rsuite';
 
+import './activities-item.css';
+
 const ActivitiesItem = props => {
   const { Line } = Progress;
   const { caption, text, progress, peoplesCount } = props;
 
   return (
-    <Panel bordered header={caption}>
+    <Panel bordered header={caption} className="activities_item">
       <FlexboxGrid justify="space-between">
         <FlexboxGrid.Item colspan={11}>
           <p>{text}</p>
