d7a11b51910844f638b07e9508c1060b9504bdf3
Andrey <andrey.grasev@gmail.com>
2021-05-16 12:36:20 +0700

    edited interface activity detaild

--- front/src/components/activity-details/activity-details.js
+++ front/src/components/activity-details/activity-details.js
@@ -1,20 +1,95 @@
 import React from 'react';
-import { Panel, Progress, Icon, FlexboxGrid } from 'rsuite';
+// import { toJS } from 'mobx';
+import {
+  Panel,
+  Progress,
+  Icon,
+  FlexboxGrid,
+  Whisper,
+  Popover,
+  ControlLabel,
+  FormControl,
+  FormGroup,
+  Form,
+  Button,
+  IconButton,
+} from 'rsuite';
+
+import './activity-details.css';
+
+const userList = [
+  {
+    username: 'andrey',
+    records: [
+      { value: 18, timestamp: 452345 },
+      { value: 20, timestamp: 3245224 },
+    ],
+  },
+  {
+    username: 'daniil',
+    records: [
+      { value: 12, timestamp: 452345 },
+      { value: 85, timestamp: 3245224 },
+    ],
+  },
+];
 
 const ActivityDetails = ({ caption, text, target }) => {
+  const { Circle } = Progress;
+  // let userList = toJS(users);
+  // console.log(users);
+  console.log(userList);
+
   return (
-    // <h1>DKFSOIDGNORGNOSNG</h1>
-    <Panel bordered header={caption} className="activities_item">
+    <Panel bordered header={caption} className="activity_details">
+      <IconButton color="red" className="delete-button" size="lg" icon={<Icon icon="trash-o" />} circle />
       <FlexboxGrid justify="space-between">
-        <FlexboxGrid.Item colspan={11}>
+        <FlexboxGrid.Item colspan={6}>
           <p>{text}</p>
-        </FlexboxGrid.Item>
-        <FlexboxGrid.Item colspan={1}>
           <Icon icon="peoples" />
-          {/* <span>{users.length}</span> */}
+          <span>{userList.length}</span>
+        </FlexboxGrid.Item>
+        <FlexboxGrid.Item colspan={6}>
+          {userList.map(el => {
+            return (
+              <Whisper
+                key={el.username}
+                placement="left"
+                trigger="hover"
+                speaker={
+                  <Popover title="Прогресс">
+                    <div
+                      style={{
+                        width: 120,
+                        display: 'inline-block',
+                        marginRight: 10,
+                      }}
+                    >
+                      <Circle percent={el.records[el.records.length - 1].value} strokeColor="#ffc107" />
+                    </div>
+                  </Popover>
+                }
+              >
+                <div className="people-progress">
+                  <Icon size="lg" icon="avatar" />
+                  <span style={{ fontSize: '25px' }}>{el.username}</span>
+                </div>
+              </Whisper>
+            );
+          })}
         </FlexboxGrid.Item>
       </FlexboxGrid>
-      {/* <Line percent={[target]} status="active" /> */}
+      <Form>
+        <FormGroup>
+          <ControlLabel>Ваш прогресс</ControlLabel>
+          <FormControl type="number" name="progress" />
+        </FormGroup>
+        <FormGroup>
+          <Button type="submit" appearance="primary">
+            Записать прогресс
+          </Button>
+        </FormGroup>
+      </Form>
     </Panel>
   );
 };
